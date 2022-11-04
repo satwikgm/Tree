8278df4b5048f28aab9a83798ad7cbaf0d7c636e
Satwik <97330754+satwikgm@users.noreply.github.com>
2022-11-04 21:38:45 +0530

    Create con_bst_pre.cpp

---
+++ con_bst_pre.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int preIndex=0;
+    TreeNode* fun(vector<int>& pre , vector<int> &in , int s , int e)
+    {
+        if(s>e)
+        {
+            return NULL;
+        }
+        TreeNode* root = new TreeNode(pre[preIndex++]);
+        int i;
+        for(i=s;i<=e;i++)
+        {
+            if(root->val == in[i])
+            {
+                break;
+            }
+        }
+        root->left = fun(pre,in,s,i-1);
+        root->right = fun(pre,in,i+1,e);
+        return root;
+    }
+    TreeNode* bstFromPreorder(vector<int>& preorder) 
+    {
+        vector<int> inorder = preorder;
+        sort(inorder.begin(),inorder.end());
+        return fun(preorder,inorder ,0,preorder.size()-1);
+    }
+};
