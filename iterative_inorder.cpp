9bcd17ad543db3f65395432fa49bdb2518a7d39b
Satwik <97330754+satwikgm@users.noreply.github.com>
2022-11-18 18:18:06 +0530

    Update iterative_inorder.cpp

--- iterative_inorder.cpp
+++ iterative_inorder.cpp
@@ -1,1 +1,32 @@
-
+/* Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};*/
+class Solution {
+public:
+    vector<int> inOrder(Node* root)
+    {
+        vector<int> v;
+        if(!root)
+        {
+            return v;
+        }
+        stack<Node*> s;
+        Node* curr = root;
+        while(curr || !s.empty())
+        {
+            while(curr)
+            {
+                s.push(curr);
+                curr=curr->left;
+            }
+            curr = s.top();
+            s.pop();
+            v.push_back(curr->data);
+            curr=curr->right;
+        }
+        return v;
+    }
+};
